9ae202b66a4c3f1315312c28ae784862a299916b
n-harshada03 <harshdanavale2003@gmail.com>
2025-02-06 13:05:58 +0530

    spring file

---
+++ src/test/java/com/example/authentication_application/AuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
